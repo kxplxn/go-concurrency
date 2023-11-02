45fc93ad6d798a968dd44c4afe1a92c7e9557f64
kxplxn <aakaplan@proton.me>
2023-11-02 04:35:01 +0000

    0104: The sync.Map

--- orders-app/db/orders.go
+++ orders-app/db/orders.go
@@ -2,32 +2,36 @@ package db
 
 import (
 	"fmt"
+	"log"
+	"sync"
 
 	"github.com/kxplxn/go-concurrency/orders-app/models"
 )
 
-type OrderDB struct {
-	placedOrders map[string]models.Order
-}
+type OrderDB struct{ placedOrders sync.Map }
 
 // NewOrders creates a new empty order service
-func NewOrders() *OrderDB {
-	return &OrderDB{
-		placedOrders: make(map[string]models.Order),
-	}
-}
+func NewOrders() *OrderDB { return &OrderDB{} }
 
 // Find order for a given id, if one exists
 func (o *OrderDB) Find(id string) (models.Order, error) {
-	order, ok := o.placedOrders[id]
+	po, ok := o.placedOrders.Load(id)
 	if !ok {
 		return models.Order{}, fmt.Errorf("no order found for %s order id", id)
 	}
 
-	return order, nil
+	return toOrder(po), nil
 }
 
 // Upsert creates or updates an order in the orders DB
 func (o *OrderDB) Upsert(order models.Order) {
-	o.placedOrders[order.ID] = order
+	o.placedOrders.Store(order.ID, order)
+}
+
+func toOrder(po any) models.Order {
+	order, ok := po.(models.Order)
+	if !ok {
+		log.Fatalf("error casting %v to order", order)
+	}
+	return order
 }
