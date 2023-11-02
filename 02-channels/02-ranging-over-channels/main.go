44d2dcabf98ede0eaa0cc816ec019874fb462bde
kxplxn <aakaplan@proton.me>
2023-11-02 19:28:49 +0000

    0202: Ranging over channels

--- 02-channels/02-ranging-over-channels/main.go
+++ 02-channels/02-ranging-over-channels/main.go
@@ -5,27 +5,32 @@ import (
 	"time"
 )
 
+var greetings = []string{"Hello!", "Ciao!", "Hola!", "Hej!", "Salut!"}
+
 func main() {
 	// create a channel
 	ch := make(chan string, 1)
+
 	// start the greeter to provide a greeting
 	go greet(ch)
-	// sleep for a long time
-	time.Sleep(2 * time.Second)
-	fmt.Println("Main ready!")
-	// receive greeting
-	greeting := <-ch
-	// sleep and print
-	time.Sleep(1 * time.Second)
-	fmt.Println("Greeting received!")
-	fmt.Println(greeting)
 
+	// sleep for a long time
+	time.Sleep(1 * time.Second)
+	fmt.Println("Main ready!")
+
+	// receive greeting
+	for greeting := range ch {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Greeting received!", greeting)
+	}
 }
 
 // greet writes a greet to the given channel and then says goodbye
 func greet(ch chan<- string) {
-	fmt.Printf("Greeter ready!\nGreeter waiting to send greeting...\n")
-	// greet
-	ch <- "Hello, world!"
+	fmt.Printf("Greeter ready!")
+	for _, g := range greetings {
+		ch <- g
+	}
+	close(ch)
 	fmt.Println("Greeter completed!")
 }
