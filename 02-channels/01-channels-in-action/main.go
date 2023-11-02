7bc7c19884333cd90069fe5716f00f9afce2eb0f
Atlas Kaplan <atlas.kaplan@lexisnexis.com>
2023-11-02 13:42:47 +0000

    0201: Channels in action

--- 02-channels/01-channels-in-action/main.go
+++ 02-channels/01-channels-in-action/main.go
@@ -7,23 +7,23 @@ import (
 
 func main() {
 	// create a channel
-	ch := make(chan string)
+	ch := make(chan string, 1)
 	// start the greeter to provide a greeting
 	go greet(ch)
 	// sleep for a long time
-	time.Sleep(5 * time.Second)
+	time.Sleep(2 * time.Second)
 	fmt.Println("Main ready!")
 	// receive greeting
 	greeting := <-ch
 	// sleep and print
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
 	fmt.Println("Greeting received!")
 	fmt.Println(greeting)
 
 }
 
 // greet writes a greet to the given channel and then says goodbye
-func greet(ch chan string) {
+func greet(ch chan<- string) {
 	fmt.Printf("Greeter ready!\nGreeter waiting to send greeting...\n")
 	// greet
 	ch <- "Hello, world!"
